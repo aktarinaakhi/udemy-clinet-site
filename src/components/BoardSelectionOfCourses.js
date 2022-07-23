9838a747ae521d500a33033e32f72d0038e248de
aktarinaakhi <aktarinacsehstu@gmail.com>
2022-07-24 02:18:38 +0600

    added headless js for tabs in board selection of courses

--- src/components/BoardSelectionOfCourses.js
+++ src/components/BoardSelectionOfCourses.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Tab } from '@headlessui/react'
 import Python from './courses/Python'
 
 export default function BoardSelectionOfCourses() {
@@ -10,34 +10,25 @@ export default function BoardSelectionOfCourses() {
                 <h1 className='text-3xl font-bold mt-11 '>A broad selection of courses</h1>
                 <h4 className='text-lg mt-2 footer-logo'>Choose from 185,000 online video courses with new additions published every month</h4>
 
-                <div className='text-lg font-semibold footer-logo mt-5'>
 
-                    <ul className="flex text-gray-600">
-                        <li className="mr-6">
-                            <Link to="/python">
-                                <span className=" hover:text-black" href="python">Python</span>
-                            </Link>
-                        </li>
-                        <li className="mr-6">
-                            <a className=" hover:text-black" href="#">Excel</a>
-                        </li>
-                        <li className="mr-6">
-                            <a className=" hover:text-black" href="#">Web Deveboplent</a>
-                        </li>
-                        <li className="mr-6">
-                            <a className="cursor-not-allowed" href="#">Javascript</a>
-                        </li>
-                        <li className="mr-6">
-                            <a className="cursor-not-allowed" href="#">Data Science</a>
-                        </li>
-                        <li className="mr-6">
-                            <a className="cursor-not-allowed" href="#">AWS Certification</a>
-                        </li>
-                        <li className="mr-6">
-                            <a className="cursor-not-allowed" href="#">Drowing</a>
-                        </li>
-                    </ul>
-                </div>
+                <Tab.Group>
+                    <Tab.List className="text-gray-600 text-lg font-semibold footer-logo mt-4">
+                        <Tab className="mr-6">Python</Tab>
+                        <Tab className="mr-6">Excel</Tab>
+                        <Tab className="mr-6">Javascript</Tab>
+                        <Tab className="mr-6">Data Science</Tab>
+                        <Tab className="mr-6">AWS Certification</Tab>
+                        <Tab className="mr-6">Drowing</Tab>
+                    </Tab.List>
+                    <Tab.Panels>
+                        <Tab.Panel className="border px-8 py-10 my-4"><Python></Python></Tab.Panel>
+                        <Tab.Panel>Content 2</Tab.Panel>
+                        <Tab.Panel>Content 3</Tab.Panel>
+                        <Tab.Panel>Content 3</Tab.Panel>
+                        <Tab.Panel>Content 3</Tab.Panel>
+                        <Tab.Panel>Content 3</Tab.Panel>
+                    </Tab.Panels>
+                </Tab.Group>
             </div>
 
 
--- src/components/courses/Python.js
+++ src/components/courses/Python.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 
 
 export default function Python() {
@@ -12,9 +14,42 @@ export default function Python() {
 
     console.log(courses);
     return (
-        <div className='text-md'>Python
+        <div className='footer-logo'>
+            <h1 className='text-2xl font-semibold'>Expand your career opportunities with Python</h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui animi sit nostrum ut totam, iste aut rerum sunt incidunt repellat fugit, consequatur eveniet minima voluptate doloribus fuga sint unde! Perspiciatis!</p>
+            <p className='my-3'>Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to...</p>
+            <button className='border border-black py-2 px-3 font-semibold'>Explore Python</button>
+
+            <div>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    slidesPerGroup={3}
+                    centeredSlides={true}
+                    loop={true}
+                    grabCursor={true}
+                    loopFillGroupWithBlank={true}
+                    pagination={{
+
+                        "clickable": true
+                    }}
+                    navigation={true}
+                    className="mySwiper">
+                    {courses.map(course => <SwiperSlide
+                        key={course.id}
+                        className='hover:shadow-lg my-4 w-96'
+                    >
+                        <div>
+                            <img src={course.img} alt="" className='mb-3 h-64' />
+                            <div className='px-3'>
+                                <p>{course.name.slice(0, 30)}</p>
+                                <p>{course.description}</p>
+                                <p>{course.price}</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>)}
+                </Swiper>
+            </div>
         </div>
     )
 }
